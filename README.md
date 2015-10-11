1ff864861e690d274c4e3262ce7618deedcad1d7
dreaddy <dreaddymck@gmail.com>
2015-10-11 13:09:58 -0500

    Add output option\n Escape command arguments \nUpdate readme.

--- README.md
+++ README.md
@@ -1,23 +1,24 @@
 # wavformgen.php
-A simple php script that calls sox, wav2png, wav2json to generate waveform png and json documents from mp3 files.
+A simple php script that combines sox, wav2png, wav2json to generate wavform png and/or wavform json documents from mp3 files.
 
 Dependencies:  sox, wav2png, wav2json
 -------------------------------------
 Usage: 
 	
-	php wavformgen.php 'song.mp3' 'foreground color', 'background color'
+	php wavformgen.php 'song.mp3' 'png|json' 'foreground color', 'background color'
 	
-	php wavformgen.php 'path/to/directory/' 'foreground color', 'background color'	
+	php wavformgen.php 'path/to/directory/' 'png|json' 'foreground color', 'background color'	
 
 	
 required:
 ------------
-Parameter 1 - "path/to/song.mp3" or "path/to/directory/" 	
+Parameter 1 - "path/to/song.mp3" or "path/to/directory/" 
+Parameter 2 - "png|json"	
 	
 optional parameters:
 
-Parameter 2 - foreground color. Defaults to 'ffb400aa' (weird yellow)
-Parameter 3 - background color. Defaults to '00000000' (transparent)	
+Parameter 3 - foreground color. Defaults to 'ffb400aa' (weird orange yellow)
+Parameter 4 - background color. Defaults to '00000000' (transparent)	
 
 Outputs to the same directory as input.
 
@@ -26,5 +27,3 @@ Output examples can be seen here: http://dreaddymck.com
 Known issues:
 -------------
 Crappy code. Yep. I know.
-
-Fails to process mp3 files with (') in the file name.
--- wavformgen.php
+++ wavformgen.php
@@ -8,29 +8,37 @@
  */
 //phpinfo();
 
-$arg = isset( $_SERVER["argv"][1] ) ? $_SERVER["argv"][1] : "";
+$target     = isset( $_SERVER["argv"][1] ) ? $_SERVER["argv"][1] : "";
+$output     = isset( $_SERVER["argv"][2] ) ? $_SERVER["argv"][2] : '';
+$forecolor  = isset( $_SERVER["argv"][3] ) ? $_SERVER["argv"][3] : 'ffb400aa';
+$backcolor  = isset( $_SERVER["argv"][4] ) ? $_SERVER["argv"][4] : '00000000';
 
 
-if( file_exists("$arg") ) {
 
-	if(is_dir("$arg")){
+if( file_exists("$target") && $output ) {
 
-		if ($handle = opendir("$arg")) {
+	if(is_dir("$target")){
+
+		if ($handle = opendir("$target")) {
 		    
-		    $count = 1;
-		    
+		    $count    = 1;
+            $loc      = "";
+            		    
 		    while (false !== ($file = readdir($handle)))
 		    {
 		        if ($file != "." && $file != ".." && strtolower(substr($file, strrpos($file, '.') + 1)) == 'mp3')
 		        {
+		            $loc = "$target/$file";
+		            error_log($loc);
 		            
-		            render_waveform( $arg.$file );
+		            render_waveform( $loc);
 		            
 		            exec("clear");
-		            echo ("(# $count) - Finished: ". $arg.$file."\n");
+		            echo ("(# $count) - Finished: ". $loc."\n");
 		            $count++;
 		            
 		        }
+		        
 		    }
 		    closedir($handle);
 		}
@@ -38,71 +46,89 @@ if( file_exists("$arg") ) {
 	}
 	else
 	{
-	    render_waveform($arg);
+	    render_waveform($target);
 	    exec("clear");
-	    echo ("Done processing $arg\n");
-	     
+	    echo ("Done processing $target\n");
 	}
 
 }
 else
 {
-     echo <<<EOF
-     
+    if(!$output){ $output = "Missing output target type"; }
+    if(!$target){ $target = "Missing target"; }
+    
+    echo <<<EOF
+
+
+             
 Dependencies:  sox, wav2png, wav2json
 
-Usage: php wavformgen.php 'song.mp3' 'foreground color', 'background color'
+Usage: php wavformgen.php 'song.mp3' 'png|json' 'foreground color', 'background color'
 
 Outputs to same directory as input.
+        
+$target
+    
+$output
 
 EOF;
      
 }
 
+exec("clear");
 
 exit("\nEnd\n");
 
 
-function render_waveform($arg){
+function render_waveform($target){
     
-    $sleep = 2;
-    $forecolor  = isset( $_SERVER["argv"][2] ) ? $_SERVER["argv"][2] : 'ffb400aa';
-    $backcolor  = isset( $_SERVER["argv"][3] ) ? $_SERVER["argv"][3] : '00000000';
+    $sleep = 1;
     
-    $dir    = dirname("$arg\n");
-    $base   = basename("$arg",".mp3");
+    global $forecolor;
+    global $backcolor;
+    global $output;
+    
+    $dir    = dirname("$target\n");
+    $base   = basename("$target",".mp3");
     
     $png    = $dir."/".$base.".wavform.png";
     $json   = $dir."/".$base.".wavform.json";
     $cmd    = "";
     
-    if( ! file_exists("$png") )
-    {
-        $cmd = "sox -v 0.99 '$arg' -c 2 -t wav - | wav2png --foreground-color=$forecolor --background-color=$backcolor -o '$png' /dev/stdin";
-        //error_log($cmd);
-        exec("clear");
-        exec($cmd);
-        sleep($sleep);
-    }
-    else
-    {
-        echo "skipping: $png\n";
-    }
     
-    if( ! file_exists("$json") )
-    {
+    if( preg_match('/^png/', $output ) ){
 
-        $cmd = "sox -v 0.99 '$arg' -c 2 -t wav - | wav2json -o '$json' /dev/stdin";
-        //error_log($cmd);
-        exec("clear");
-        exec($cmd);
-        sleep($sleep);
-    }
-    else
-    {
-        echo "skipping: $json\n";
+        if( ! file_exists("$png") )
+        {
+            $cmd = "sox -v 0.99 \"$target\" -c 2 -t wav - | wav2png --foreground-color=$forecolor --background-color=$backcolor -o \"$png\" /dev/stdin";
+            //error_log($cmd);
+            exec("clear");
+            exec($cmd);
+        }
+        else
+        {
+            echo "skip converting: $target to png\n";
+        }
     }
     
+
+    if( preg_match('/^json/', $output ) ){
+        
+        if( ! file_exists("$json") )
+        {
+            $cmd = "sox -v 0.99 \"$target\" -c 2 -t wav - | wav2json -o \"$json\" /dev/stdin";
+            //error_log($cmd);
+            exec("clear");
+            exec($cmd);
+        }
+        else
+        {
+            echo "skip converting: $target to json\n";
+        }
+    }
+
+    
+    sleep($sleep);
 }
 
 ?>
